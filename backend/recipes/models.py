85c349a94c05039faa87c62d6cc134140c7929e5
humanpride <pashservl@ya.ru>
2026-01-02 18:16:46 +0300

    feat: add users models and remake migrations

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,1 +1,5 @@
-# Create your models here.
+from django.db import models
+
+
+class Recipe(models.Model):
+    pass
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,1 +1,100 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from recipes.models import Recipe
+
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = (
+        (USER, 'User'),
+        (ADMIN, 'Admin'),
+    )
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email',
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        blank=True,
+        null=True,
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=USER,
+    )
+
+    def __str__(self):
+        return self.username
+
+    def is_admin(self) -> bool:
+        return self.role == self.ADMIN
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='subscriptions',
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='subscribers',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_subscription',
+            ),
+        ]
+
+    def __str__(self):
+        return f'{self.user} → {self.author}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='favorites',
+        on_delete=models.CASCADE,
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='favorited_by',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_favorite',
+            ),
+        ]
+
+
+class ShoppingCartItem(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='shopping_cart',
+        on_delete=models.CASCADE,
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='in_shopping_carts',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_shopping_cart_item',
+            ),
+        ]
