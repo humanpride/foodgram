75cb12829ed88d11a4447bc9616f8dfb1b00dec7
humanpride <pashservl@ya.ru>
2026-01-02 21:46:12 +0300

    feat: add ingredients to admin panel

--- backend/ingredients/admin.py
+++ backend/ingredients/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Ingredient
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    search_fields = ('name',)
+    ordering = ('name',)
--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -1,1 +1,21 @@
-# Create your models here.
+from django.db import models
+
+
+class Ingredient(models.Model):
+    MEASUREMENT_UNITS = (
+        ('граммы', 'г'),
+        ('милилитры', 'мл'),
+        ('штуки', 'шт.'),
+        ('столовые ложки', 'ст.л.'),
+        ('чайные ложки', 'ч.л.'),
+    )
+
+    name = models.CharField(
+        'Название',
+        max_length=20,
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=10,
+        choices=MEASUREMENT_UNITS,
+    )
