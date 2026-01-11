5d4189df8a75fe58060ac6785a8acde53755a148
humanpride <pashservl@ya.ru>
2026-01-11 19:13:44 +0300

    refactor: RecipeFilter (because new RecipeViewSet get_queryset)

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -2,7 +2,6 @@ from django_filters import BaseInFilter, CharFilter
 from django_filters import rest_framework as filters
 
 from recipes.models import Recipe
-from users.models import Favorite, ShoppingCartItem
 
 
 class CharInFilter(BaseInFilter, CharFilter):
@@ -10,9 +9,9 @@ class CharInFilter(BaseInFilter, CharFilter):
 
 
 class RecipeFilter(filters.FilterSet):
-    is_favorited = filters.NumberFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.NumberFilter(
-        method='filter_is_in_shopping_cart'
+    is_favorited = filters.BooleanFilter(field_name='is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        field_name='is_in_shopping_cart'
     )
     author = filters.NumberFilter(field_name='author__id')
     tags = CharInFilter(field_name='tags__slug', lookup_expr='in')
@@ -20,43 +19,3 @@ class RecipeFilter(filters.FilterSet):
     class Meta:
         model = Recipe
         fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
-
-    def filter_is_favorited(self, qs, name, value):
-        user = getattr(self.request, 'user', None)
-        if str(value) == '1':
-            if user and user.is_authenticated:
-                return qs.filter(
-                    pk__in=Favorite.objects.filter(user=user).values_list(
-                        'recipe_id', flat=True
-                    )
-                )
-            return qs.none()
-        if str(value) == '0':
-            if user and user.is_authenticated:
-                return qs.exclude(
-                    pk__in=Favorite.objects.filter(user=user).values_list(
-                        'recipe_id', flat=True
-                    )
-                )
-            return qs
-        return qs
-
-    def filter_is_in_shopping_cart(self, qs, name, value):
-        user = getattr(self.request, 'user', None)
-        if str(value) == '1':
-            if user and user.is_authenticated:
-                return qs.filter(
-                    pk__in=ShoppingCartItem.objects.filter(
-                        user=user
-                    ).values_list('recipe_id', flat=True)
-                )
-            return qs.none()
-        if str(value) == '0':
-            if user and user.is_authenticated:
-                return qs.exclude(
-                    pk__in=ShoppingCartItem.objects.filter(
-                        user=user
-                    ).values_list('recipe_id', flat=True)
-                )
-            return qs
-        return qs
