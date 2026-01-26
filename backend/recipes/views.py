fa42216f714c59d8c954bf46f5c444346e61a759
humanpride <pashservl@ya.ru>
2026-01-26 12:01:28 +0300

    fix: recipes views file

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect
-from rest_framework.serializers import ValidationError
 
 from recipes.models import Recipe
 
@@ -7,6 +7,6 @@ from recipes.models import Recipe
 def recipe_short_redirect(request, recipe_id):
     if not Recipe.objects.filter(id=recipe_id).exists():
         raise ValidationError(
-            {'recipe_id': ['Рецепта с id {recipe_id} не найдено.']}
+            {'recipe_id': [f'Рецепта с id {recipe_id} не найдено.']}
         )
     return redirect(f'/recipes/{recipe_id}/')
